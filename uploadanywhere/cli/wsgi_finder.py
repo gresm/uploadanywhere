7889d8da7b00338a60a9a7c6e4b1775f4ca34953
gresm <78505251+gresm@users.noreply.github.com>
2023-01-24 13:51:02 +0000

    WSGIFinder use BaseFinder

--- uploadanywhere/cli/wsgi_finder.py
+++ uploadanywhere/cli/wsgi_finder.py
@@ -33,80 +33,78 @@ __all__ = [
 
 
 from pathlib import Path as _Path
-import sys
+# import sys
 
-from .tools import test_path, warning, if_test, error
+
+from .base_finder import BaseFinder
 
 
 WSGI_DEFAULT_DIR = _Path("/var/www")
 
 
-class WSGIFinder:
+class WSGIFinder(BaseFinder):
     """WSGI Finder class"""
 
-    def __init__(self, wsgi_file: _Path | None = None, wsgi_dir: _Path | None = None):
-        self.file: _Path | None = wsgi_file
-        self.wsgi_dir = wsgi_dir or WSGI_DEFAULT_DIR
+    DEFAULT_SEARCH_DIR = WSGI_DEFAULT_DIR
 
-    def found(self):
-        """Return True if WSGI file found, False otherwise."""
-        return self.file is not None
+    searching_for = "wsgi file"
+    searching_for_file = True
 
-    def cli_manual_selection(self):
-        """Manually select wsgi file path."""
+    # def cli_manual_selection(self):
+    #     """Manually select wsgi file path."""
 
-        if self.found():
-            return
+    #     if self.found():
+    #         return
 
-        if if_test("Proceed with manual configuration?"):
-            path = _Path(input("Enter valid path for wsgi python file: "))
-            test_path(path, True)
-            self.file = path
-        else:
-            sys.exit(0)
+    #     if if_test("Proceed with manual configuration?"):
+    #         path = _Path(input("Enter valid path for wsgi python file: "))
+    #         test_path(path, True)
+    #         self.file = path
+    #     else:
+    #         sys.exit(0)
 
-    def cli_select_from_dir(self):
-        """Choose between files in wsgi directory"""
+    # def cli_select_from_dir(self):
+    #     """Choose between files in wsgi directory"""
 
-        if self.found():
-            return
+    #     if self.found():
+    #         return
 
-        possible = []
+    #     possible = []
 
-        for possible_path in self.wsgi_dir.iterdir():
-            if possible_path.is_file() and possible_path.suffix == ".py":
-                possible.append(possible_path)
+    #     for possible_path in self.path.iterdir():
+    #         if possible_path.is_file() and possible_path.suffix == ".py":
+    #             possible.append(possible_path)
 
-        if len(possible) == 0:
-            warning("WSGI default setup folder empty.")
-            self.cli_manual_selection()
-        elif len(possible) == 1:
-            if if_test(f"Use {str(possible[0])}?"):
-                self.file = possible[0]
-            else:
-                self.cli_manual_selection()
+    #     if len(possible) == 0:
+    #         warning("WSGI default setup folder empty.")
+    #         self.cli_manual_selection()
+    #     elif len(possible) == 1:
+    #         if if_test(f"Use {str(possible[0])}?"):
+    #             self.file = possible[0]
+    #         else:
+    #             self.cli_manual_selection()
 
-        print("Possible files:")
-        for idx, file in enumerate(possible):
-            print(f"[{idx + 1}] {str(file)}")
+    #     print("Possible files:")
+    #     for idx, file in enumerate(possible):
+    #         print(f"[{idx + 1}] {str(file)}")
 
-        option = input("Select file number, or nothing for manual configuration: ")
-        if not option:
-            self.cli_manual_selection()
-        else:
-            if option.isdigit():
-                opt = int(option)
-                if not 0 < opt <= len(possible):
-                    error("Invalid index.")
-                self.file = possible[opt]
-            else:
-                error("Not a number.")
+    #     option = input("Select file number, or nothing for manual configuration: ")
+    #     if not option:
+    #         self.cli_manual_selection()
+    #     else:
+    #         if option.isdigit():
+    #             opt = int(option)
+    #             if not 0 < opt <= len(possible):
+    #                 error("Invalid index.")
+    #             self.file = possible[opt]
+    #         else:
+    #             error("Not a number.")
 
-    def cli_find(self):
+    def find(self):
         """Find wsgi file"""
         print("Searching for WSGI file to patch.")
-        if not self.wsgi_dir.exists():
-            warning("WSGI setup files folder not found.")
-            self.cli_manual_selection()
-        self.cli_select_from_dir()
-        return self.file
+        if not self.search_dir.exists():
+            self.warning("WSGI setup files folder not found.")
+            self.manual_selection()
+        self.select_file_from_dir(self.search_dir, False)
+        return self.path
